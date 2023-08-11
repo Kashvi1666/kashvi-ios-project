13498bb4a728cca7e0fcf945ee82391aeb990e35
kashvi <kashviswami@gmail.com>
2023-08-11 10:17:11 -0700

    Initial Commit

---
+++ kashvi/kashviApp.swift
@@ -0,0 +1,17 @@
+//
+//  kashviApp.swift
+//  kashvi
+//
+//  Created by Kashvi Swami on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct kashviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
